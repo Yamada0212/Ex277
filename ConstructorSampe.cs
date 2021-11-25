e1e50604833b1ac68a1cb8e5151534af8e267f20
gaito yamada <gaito021229@au.com>
2021-11-25 15:42:52 +0900

    プロジェクト ファイルを追加します。

---
+++ ConstructorSampe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConsoleUtility;
+
+namespace ConstructorSampe
+{
+    class ConstructorSampeMain
+    {
+        public static void Main(string[] args)
+        {
+            Triangle a = new Triangle(
+                InteractiveInput.InputFloat("辺の長さa："),
+                InteractiveInput.InputFloat("辺の長さb："),
+                InteractiveInput.InputFloat("辺の長さc："));
+            Console.WriteLine($"面積は{a.GetSurface()}");
+        }
+        
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Triangle
+{
+    private float[] sideLength = new float[3]; 
+
+        public Triangle(float sideA, float sideB, float sideC)
+    {
+        this.sideLength[0] = sideA;
+        this.sideLength[1] = sideB;
+        this.sideLength[2] = sideC;
+    }
+    public float GetSurface()
+    {
+        /*return width * height / 2;*/
+        float s = (sideLength[0] + sideLength[1] + sideLength[2]) / 2;
+        return MathF.Sqrt(s * (s - sideLength[0]) * (s - sideLength[1]) * (s - sideLength[2]));
+
+    }
+    
+}
